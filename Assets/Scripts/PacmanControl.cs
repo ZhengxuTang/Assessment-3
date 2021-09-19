0179616d6de476e9887c4d93f407a813fa744f44
ZhengxuTang <734335759@qq.com>
2021-09-19 22:53:21 +0800

    19/9/2021 pacman move

--- Assets/Scripts/PacmanControl.cs
+++ Assets/Scripts/PacmanControl.cs
@@ -5,36 +5,54 @@ using UnityEngine;
 public class PacmanControl : MonoBehaviour
 {
     public Animator animatorController;
+    [SerializeField]
     public GameObject Pacman;
+    private Tweener tweener;
+    public AudioSource moveMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tweener = GetComponent<Tweener>();
+
+        if (Pacman.transform.position != new Vector3 (-0.5f, 1.5f, 0.0f))
+        {
+            tweener.AddTween(Pacman.transform, Pacman.transform.position, new Vector3 (-0.5f, 1.5f, 0.0f), 0.0f);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            animatorController.SetTrigger("DownParam");
-            //Pacman.transform.rotation = Quaternion.Euler(0, 0, 180);
-        }
+        //Pacman.transform.rotation = Quaternion.Euler(0, 0, 180);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            animatorController.SetTrigger("UpParam");
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Pacman.transform.position == new Vector3 (-0.5f, 1.5f, 0.0f))
         {
             animatorController.SetTrigger("LeftParam");
+            tweener.AddTween(Pacman.transform, new Vector3 (-0.5f, 1.5f, 0.0f), new Vector3 (-5.5f, 1.5f, 0.0f), 2.0f);
+            moveMusic.Play();
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Pacman.transform.position == new Vector3 (-5.5f, 1.5f, 0.0f))
+        {
+            animatorController.SetTrigger("UpParam");
+            tweener.AddTween(Pacman.transform, Pacman.transform.position, new Vector3 (-5.5f, 5.5f, 0.0f), 2.0f);
+            moveMusic.Play();
+        }
+
+        if (Pacman.transform.position == new Vector3 (-5.5f, 5.5f, 0.0f))
         {
             animatorController.SetTrigger("RightParam");
+            tweener.AddTween(Pacman.transform, Pacman.transform.position, new Vector3 (-0.5f, 5.5f, 0.0f), 2.0f);
+            moveMusic.Play();
+        }
+
+        if (Pacman.transform.position == new Vector3 (-0.5f, 5.5f, 0.0f))
+        {
+            animatorController.SetTrigger("DownParam");
+            tweener.AddTween(Pacman.transform, Pacman.transform.position, new Vector3 (-0.5f, 1.5f, 0.0f), 2.0f);
+            moveMusic.Play();
         }
     }
 }
